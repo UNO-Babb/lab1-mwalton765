b8795e3ad6f2130702825173ef0ba2948a2a2fd1
mwalton765 <mwalton765@comcast.net>
2024-08-28 17:46:08 -0700

    Add files via upload
    manually uploading work due to commit and sync issues

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,19 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Michael J. Walton
+#Date: 8/28/2024
+#Assignment: Lab 1 First Program
 
 def main():
   print("First Program")
   #Say hello
-  
+  print('Hello')
   #Ask for the user's name
-
+  name = input('What is your name? ')
   #Use the user's name in the program.
-
+  print('Hello', name)
   #Ask the user for their age.
-
+  age = input('What is your age? ')
+  age = int(age)
+  born = 2024 - age
   #Tell the user what year they were born in.
+  print('You were born in', born)
   #Assume that they have not had their birthday yet this year.
 
 
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,28 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Michael J. Walton
+#Date: 08/28/2024
+#Assignment: Lab 1 MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
+  word1 = input('Enter the first adjective  ')
+  word2 = input('Enter the second adjective ')
+  word3 = input('Enter the third adjective  ')
+  word4 = input('Enter the fourth adjective ')
+  word5 = input('Enter the fifth adjective  ')
+  word6 = input('Enter the sixth adjective  ')
+  word7 = input('Now enter a noun           ')
 
   #Print the story with the user supplied words.
-
+  print('How to Make a Terrible Sandwich!')
+  print('First get 2 slices of', word1, 'bread')
+  print('Put on some', word2, 'pickles')
+  print('Add some', word3, 'onions')
+  print('and some', word4, 'sauce')
+  print('Then slap on some', word5, 'cheese')
+  print('and some random', word6, 'meat')
+  print('Smash it with a', word7, 'and enjoy!')
 
 
 #Call the main function if this is the file being run.
